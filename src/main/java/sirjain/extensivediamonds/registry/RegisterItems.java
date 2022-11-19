d26f37f815eec23d4dd876ec1a6975fd3028e2b1
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-11-19 17:18:54 -0500

    Make fused diamonds rare items

--- src/main/java/sirjain/extensivediamonds/registry/RegisterItems.java
+++ src/main/java/sirjain/extensivediamonds/registry/RegisterItems.java
@@ -3,6 +3,7 @@ package sirjain.extensivediamonds.registry;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 import sirjain.extensivediamonds.ExtensiveDiamonds;
 import sirjain.extensivediamonds.items.material.FusedDiamondArmor;
@@ -57,7 +58,7 @@ public class RegisterItems {
         FUSED_DIAMOND = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond"),
-                new Item(new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new Item(new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
     }
 
@@ -83,31 +84,31 @@ public class RegisterItems {
         FUSED_DIAMOND_SWORD = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_sword"),
-                new FusedDiamondSwordItem(ModToolMaterials.FUSED_DIAMOND, 13, 2f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondSwordItem(ModToolMaterials.FUSED_DIAMOND, 13, 2f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
 
         FUSED_DIAMOND_AXE = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_axe"),
-                new FusedDiamondAxeItem(ModToolMaterials.FUSED_DIAMOND, 6, 2.5f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondAxeItem(ModToolMaterials.FUSED_DIAMOND, 6, 2.5f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
 
         FUSED_DIAMOND_HOE = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_hoe"),
-                new FusedDiamondHoeItem(ModToolMaterials.FUSED_DIAMOND, 3, 2f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondHoeItem(ModToolMaterials.FUSED_DIAMOND, 3, 2f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
 
         FUSED_DIAMOND_SHOVEL = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_shovel"),
-                new FusedDiamondShovelItem(ModToolMaterials.FUSED_DIAMOND, 4, 2.5f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondShovelItem(ModToolMaterials.FUSED_DIAMOND, 4, 2.5f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
 
         FUSED_DIAMOND_PICKAXE = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_pickaxe"),
-                new FusedDiamondPickaxeItem(ModToolMaterials.FUSED_DIAMOND, 5, 2f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondPickaxeItem(ModToolMaterials.FUSED_DIAMOND, 5, 2f, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
     }
 
@@ -115,25 +116,25 @@ public class RegisterItems {
         FUSED_DIAMOND_HELMET = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_helmet"),
-                new FusedDiamondArmor(FusedDiamondArmorMaterial.material, EquipmentSlot.HEAD, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondArmor(FusedDiamondArmorMaterial.material, EquipmentSlot.HEAD, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
 
         FUSED_DIAMOND_CHEST = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_chestplate"),
-                new FusedDiamondArmor(FusedDiamondArmorMaterial.material, EquipmentSlot.CHEST, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondArmor(FusedDiamondArmorMaterial.material, EquipmentSlot.CHEST, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
 
         FUSED_DIAMOND_LEGS = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_leggings"),
-                new FusedDiamondArmor(FusedDiamondArmorMaterial.material, EquipmentSlot.LEGS, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondArmor(FusedDiamondArmorMaterial.material, EquipmentSlot.LEGS, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
 
         FUSED_DIAMOND_BOOTS = Registry.register(
                 Registry.ITEM,
                 new Identifier(ExtensiveDiamonds.MOD_ID, "fused_diamond_boots"),
-                new FusedDiamondArmor(FusedDiamondArmorMaterial.material, EquipmentSlot.FEET, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP))
+                new FusedDiamondArmor(FusedDiamondArmorMaterial.material, EquipmentSlot.FEET, new Item.Settings().group(ExtensiveDiamonds.EXTENSIVEDIAMONDS_ITEM_GROUP).rarity(Rarity.RARE))
         );
     }
 }
