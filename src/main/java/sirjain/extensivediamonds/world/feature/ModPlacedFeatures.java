337d359eb514d2f30e029eda5d7a98401549be25
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-03-07 18:09:17 -0500

    Revert "(Broken) world gen"
    This reverts commit 818658379e846affcc14b1a7623a27233e8c1656.

--- src/main/java/sirjain/extensivediamonds/world/feature/ModPlacedFeatures.java
+++ src/main/java/sirjain/extensivediamonds/world/feature/ModPlacedFeatures.java
@@ -1,48 +1,28 @@
 package sirjain.extensivediamonds.world.feature;
 
-import net.minecraft.registry.Registerable;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.PlacedFeature;
+import net.minecraft.world.gen.feature.PlacedFeatures;
 import net.minecraft.world.gen.placementmodifier.*;
-import sirjain.extensivediamonds.ExtensiveDiamonds;
 
 import java.util.List;
 
 public class ModPlacedFeatures {
-    public static final RegistryKey<PlacedFeature> RED_DIAMOND_ORE_PLACED_KEY = registerKey("red_diamond_ore_placed");
-    public static final RegistryKey<PlacedFeature> GREEN_DIAMOND_ORE_PLACED_KEY = registerKey("green_diamond_ore_placed");
-    public static final RegistryKey<PlacedFeature> DARK_DIAMOND_ORE_PLACED_KEY = registerKey("dark_diamond_ore_placed");
+    public static final RegistryEntry<PlacedFeature> RED_DIAMOND_ORE_PLACED = PlacedFeatures.register("red_diamond_ore_placed",
+            ModConfiguredFeatures.RED_DIAMOND_ORE, modifiersWithCount(5,
+                    HeightRangePlacementModifier.trapezoid(YOffset.fixed(-50), YOffset.fixed(-59))));
 
-    public static void bootstrap(Registerable<PlacedFeature> context) {
-        var configuredFeatureRegistryEntryLookup = context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
+    public static final RegistryEntry<PlacedFeature> GREEN_DIAMOND_ORE_PLACED = PlacedFeatures.register("green_diamond_ore_placed",
+            ModConfiguredFeatures.GREEN_DIAMOND_ORE, modifiersWithCount(5,
+                    HeightRangePlacementModifier.trapezoid(YOffset.fixed(-50), YOffset.fixed(-59))));
 
-        register(context, RED_DIAMOND_ORE_PLACED_KEY, configuredFeatureRegistryEntryLookup.getOrThrow(ModConfiguredFeatures.RED_DIAMOND_ORE_KEY),
-                modifiersWithCount(16, // VeinsPerChunk
-                        HeightRangePlacementModifier.trapezoid(YOffset.aboveBottom(-80), YOffset.aboveBottom(80))));
-        register(context, GREEN_DIAMOND_ORE_PLACED_KEY, configuredFeatureRegistryEntryLookup.getOrThrow(ModConfiguredFeatures.GREEN_DIAMOND_ORE_KEY),
-                modifiersWithCount(16, // VeinsPerChunk
-                        HeightRangePlacementModifier.uniform(YOffset.aboveBottom(-80), YOffset.aboveBottom(80))));
-        register(context, DARK_DIAMOND_ORE_PLACED_KEY, configuredFeatureRegistryEntryLookup.getOrThrow(ModConfiguredFeatures.DARK_DIAMOND_ORE_KEY),
-                modifiersWithCount(16, // VeinsPerChunk
-                        HeightRangePlacementModifier.uniform(YOffset.aboveBottom(-80), YOffset.aboveBottom(80))));
-    }
+    public static final RegistryEntry<PlacedFeature> DARK_DIAMOND_ORE_PLACED = PlacedFeatures.register("dark_diamond_ore_placed",
+            ModConfiguredFeatures.DARK_DIAMOND_ORE, modifiersWithCount(4,
+                    HeightRangePlacementModifier.trapezoid(YOffset.fixed(-50), YOffset.fixed(-59))));
 
     private static List<PlacementModifier> modifiers(PlacementModifier countModifier, PlacementModifier heightModifier) {
         return List.of(countModifier, SquarePlacementModifier.of(), heightModifier, BiomePlacementModifier.of());
-    }
-
-    public static RegistryKey<PlacedFeature> registerKey(String name) {
-        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(ExtensiveDiamonds.MOD_ID, name));
-    }
-
-    private static void register(Registerable<PlacedFeature> context, RegistryKey<PlacedFeature> key, RegistryEntry<ConfiguredFeature<?, ?>> configuration,
-                                 List<PlacementModifier> modifiers) {
-        context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
     }
 
     private static List<PlacementModifier> modifiersWithCount(int count, PlacementModifier heightModifier) {
