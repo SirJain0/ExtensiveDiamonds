5eabf659ba9df6b7bc12146f32bb45025ad03e44
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-06-12 17:37:38 -0400

    Update to 1.20.1

--- src/main/java/sirjain/extensivediamonds/screen/GemChargerScreen.java
+++ src/main/java/sirjain/extensivediamonds/screen/GemChargerScreen.java
@@ -1,9 +1,9 @@
 package sirjain.extensivediamonds.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -22,31 +22,29 @@ public class GemChargerScreen extends HandledScreen<GemChargerScreenHandler> {
         titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
     }
 
-    @Override
-    protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
+    protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexProgram);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, GUI_TEXTURE);
         int x = (width - backgroundWidth) / 2;
         int y = (height - backgroundHeight) / 2;
-        drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
+        context.drawTexture(GUI_TEXTURE, x, y, 0, 0, backgroundWidth, backgroundHeight);
 
-        renderProgressArrow(matrices, x, y);
+        renderProgressArrow(context, x, y);
     }
 
-    private void renderProgressArrow(MatrixStack stack, int x, int y) {
-        if(handler.isCrafting()) {
-            drawTexture(stack, x + 61, y + 39, 0, 171, handler.getScaledProgress(), 13);
+    private void renderProgressArrow(DrawContext context, int x, int y) {
+        if (handler.isCrafting()) {
+            context.drawTexture(GUI_TEXTURE, x + 61, y + 39, 0, 171, handler.getScaledProgress(), 13);
             // u and v = coordinates of progress bar texture
             // x and y = coordinates of starting point
             // height = height of progress bar's dimensions
         }
     }
 
-    @Override
-    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float delta) {
-        renderBackground(matrixStack);
-        super.render(matrixStack, mouseX, mouseY, delta);
-        drawMouseoverTooltip(matrixStack, mouseX, mouseY);
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        renderBackground(context);
+        super.render(context, mouseX, mouseY, delta);
+        drawMouseoverTooltip(context, mouseX, mouseY);
     }
 }
