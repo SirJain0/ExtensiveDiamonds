76dd15e88ba8fe75d6ad9d4249cce6bd6c9d7c26
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-10-02 20:04:42 -0400

    Make fused diamond swords only occasionally light enemies on fire and heal the attacker

--- src/main/java/sirjain/extensivediamonds/items/swords/FusedDiamondSwordItem.java
+++ src/main/java/sirjain/extensivediamonds/items/swords/FusedDiamondSwordItem.java
@@ -17,8 +17,9 @@ public class FusedDiamondSwordItem extends SwordItem {
 	public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 		target.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, 3 * 20, 1), attacker);
 		target.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 4 * 20, 2), attacker);
-		target.setOnFireFor(5);
-		attacker.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 20, 1), attacker);
+
+		if (target.getRandom().nextInt(2) == 0) target.setOnFireFor(5);
+		if (target.getRandom().nextInt(3) == 0) attacker.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 20, 1), attacker);
 
 		return super.postHit(stack, target, attacker);
 	}
