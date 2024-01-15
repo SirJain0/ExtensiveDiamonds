ab696ae1cc76a5650f42f35704e917fd9ba118e4
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-01-14 20:50:01 -0500

    Improve full armor effect system
    Make any living entity be able to get full armor effects

--- src/main/java/sirjain/extensivediamonds/items/material/FusedDiamondArmor.java
+++ src/main/java/sirjain/extensivediamonds/items/material/FusedDiamondArmor.java
@@ -1,11 +1,13 @@
 package sirjain.extensivediamonds.items.material;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -18,41 +20,26 @@ public class FusedDiamondArmor extends ArmorItem {
 	public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
 		if (
 			!world.isClient() &&
-			entity instanceof PlayerEntity player &&
-			checkForWearingArmor(player) &&
-			checkForCorrectArmor(player)
+			entity instanceof LivingEntity wearer &&
+			isWearingFusedDiamondArmor(wearer)
 		) {
-			player.addStatusEffect(new StatusEffectInstance(StatusEffects.LUCK, 20 * 3, 1));
-			player.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 20 * 3, 0));
-			player.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 20 * 3, 0));
+			wearer.addStatusEffect(new StatusEffectInstance(StatusEffects.LUCK, 20 * 3, 1));
+			wearer.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 20 * 3, 0));
+			wearer.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 20 * 3, 0));
 		}
 
 		super.inventoryTick(stack, world, entity, slot, selected);
 	}
 
-	// Check 1: Is the player wearing armor?
-	private boolean checkForWearingArmor(PlayerEntity player) {
-		ItemStack head = player.getInventory().getArmorStack(0);
-		ItemStack chest = player.getInventory().getArmorStack(1);
-		ItemStack legs = player.getInventory().getArmorStack(2);
-		ItemStack boots = player.getInventory().getArmorStack(3);
+	private boolean isWearingFusedDiamondArmor(LivingEntity wearer) {
+		Item head = wearer.getEquippedStack(EquipmentSlot.HEAD).getItem();
+		Item chest = wearer.getEquippedStack(EquipmentSlot.CHEST).getItem();
+		Item legs = wearer.getEquippedStack(EquipmentSlot.LEGS).getItem();
+		Item boots = wearer.getEquippedStack(EquipmentSlot.FEET).getItem();
 
-		return !chest.isEmpty() &&
-			!head.isEmpty() &&
-			!legs.isEmpty() &&
-			!boots.isEmpty();
-	}
-
-	// Check 2: Is said armor the correct material?
-	private boolean checkForCorrectArmor(PlayerEntity player) {
-		ArmorItem head = (ArmorItem) player.getInventory().getArmorStack(0).getItem();
-		ArmorItem chest = (ArmorItem) player.getInventory().getArmorStack(1).getItem();
-		ArmorItem legs = (ArmorItem) player.getInventory().getArmorStack(2).getItem();
-		ArmorItem boots = (ArmorItem) player.getInventory().getArmorStack(3).getItem();
-
-		return chest.getMaterial() == FusedDiamondArmorMaterial.MATERIAL &&
-			head.getMaterial() == FusedDiamondArmorMaterial.MATERIAL &&
-			legs.getMaterial() == FusedDiamondArmorMaterial.MATERIAL &&
-			boots.getMaterial() == FusedDiamondArmorMaterial.MATERIAL;
+		return head instanceof ArmorItem headItem && headItem.getMaterial() == FusedDiamondArmorMaterial.MATERIAL &&
+			chest instanceof ArmorItem chestItem && chestItem.getMaterial() == FusedDiamondArmorMaterial.MATERIAL &&
+			legs instanceof ArmorItem legItem && legItem.getMaterial() == FusedDiamondArmorMaterial.MATERIAL &&
+			boots instanceof ArmorItem bootItem && bootItem.getMaterial() == FusedDiamondArmorMaterial.MATERIAL;
 	}
 }
